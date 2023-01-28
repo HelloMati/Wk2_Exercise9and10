354c54c89265880d67cfdece6af9e5567fe38e6c
Eyasmin Basher <eyasminbasher@gmail.com>
2023-01-28 19:28:05 +0000

    exercise 9 submission

---
+++ Exercise9/types_practice.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+
+first_name = "Eyasmin"
+last_name = "Basher"
+
+print(first_name + " " + last_name)
+
+name_list = [first_name, last_name]
+
+name_dict = {"first": first_name, "last": last_name}
+print(name_list)
+print(name_dict)
+print(type(name_list))
+print(type(name_dict))
