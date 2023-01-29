8c75d1cfe0c73c0901f9918e38b8a3dbf05fa39c
Sara <saesbori@gmail.com>
2023-01-29 19:18:12 +0000

    add comment explaining difference between isdigit() and isdecimal()

--- Exercise9/operations.py
+++ Exercise9/operations.py
@@ -1,8 +1,9 @@
-var = input("Please enter a value: ")
-print(f'Print of the input all in upper case: {var.upper()}')  # method returns a string where all characters are in upper case.
+var = input("Please enter a value: ")  # input() prompts the console with the string given and takes user input
+print(f'Print of the input all in upper case: {var.upper()}')  # str.upper() returns the str with all upper case
 print(f'Number of characters: {len(var)}')  # function returns the number of items in an object.
-print(f'Are all characters numbers? Using .isdigit(): {var.isdigit()}')  # method returns True if all the characters are digits, otherwise False.
-print(f'Are all characters numbers? Using .isdecimal():{var.isdecimal()}')  # method checks whether the string consists only decimals (0-9) or not.
+# Difference between .isdigit() and .isdecimal() explained below
+print(f'Are all characters numbers? Using .isdigit(): {var.isdigit()}')  # str.isdigit() returns True is all numbers
+print(f'Are all characters numbers? Using .isdecimal():{var.isdecimal()}')  # str.isdigit() returns True is all numbers
 
 # Using loop to check every character of the input and assess if is a number
 numeric = False
@@ -18,5 +19,17 @@ for char in var:
         break  # No need to continue checking more characters as we already found a number
 print(f'Does it contain any numeric characters? isdecimal(): {numeric}')
 
-### COMMENT: difference between isdigit() VS isdecimal()
 
+# COMMENT: difference between isdigit() VS isdecimal()
+# The main difference between them is the type of character they will be able to check.
+
+# isdigit() returns True if the string is formed by:
+# → Base 10 numbers (0123456789)
+# → Superscripts (2²)
+# Will return False for: Roman numerals and fractions in unicode and for whitespaces or decimals.
+
+# isdecimal() returns True if the string is formed by a Base 10 number. Any other string (e.g. fractions) is False.
+
+print("\nTrying out differences between isdigit() and isdecimal()")
+print(f'20 → isdigit: {"20".isdigit()} // isdecimal: {"20".isdecimal()}')
+print(f'2² → isdigit: {"2²".isdigit()} // isdecimal: {"2²".isdecimal()}')
