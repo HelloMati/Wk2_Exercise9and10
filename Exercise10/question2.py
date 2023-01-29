85f9c10d3a54409b7fe27a67f769541c458b06f1
Sara <saesbori@gmail.com>
2023-01-29 16:50:29 +0000

    Exercise 10 - Question 2: breakdown problem steps

---
+++ Exercise10/question2.py
@@ -0,0 +1,17 @@
+
+# Create a variable to store the hard coded pin (pin to check against)
+
+
+# Create a variable to store the inputted pin by the user
+supplied_pin = input("Enter your PIN: ")
+
+
+# Create a variable to store the maximum of attempts to check pin. Set it to 3
+
+
+# For each attempt output a failure/success message.
+# Must include no attempts left
+# Use if/else statements to check failure/success
+# while loop to account for the number of attemps
+
+
